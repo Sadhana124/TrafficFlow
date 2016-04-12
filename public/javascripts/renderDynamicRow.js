26b9bb9df834c97b102f9cf73255592f885ceafb
Sadhana124 <sadhana.412@gmail.com>
2016-04-12 12:25:04 -0700

    dynamic adding of rows

--- public/javascripts/renderDynamicRow.js
+++ public/javascripts/renderDynamicRow.js
@@ -8,12 +8,11 @@ function addRow(frm) {
     var row =
         '<div class="form-group row"> ' +
             '<div class = "col-xs-3"> ' +
-                '<select name="condAttr-1" class = "form-control">' +
-                    '<% for(var i=0; i<availableAttributes.length; i++) { %>' +
-                        '<option value=<%= availableAttributes[i]%>>' +
-                        '<%= availableAttributes[i]%></option> ' +
-                    '<% } %> ' +
-                '</select>' +
+
+        '<select id="row' + rowNum + '">' +
+        '<% for(i=0 ; i<availableAttributes.length; i++) { %>' +
+        '<option value=<%= availableAttributes[i]%>><%= availableAttributes[i]%></option>' +
+        '<% } %>  </select>' +
 '</div> ' +
         '<div class = "col-xs-3"> ' +
         '<select name="op-1" class = "form-control"> ' +
@@ -34,7 +33,12 @@ function addRow(frm) {
         '</div>' +
 '</div>';
 
-    jQuery('#formContainer').append(row);
+
+    var row2 = '<div class="form-group row"><div class = "col-xs-3"><select name=“condAttr-1" class = "form-control"><% for(var i=0; i<availableAttributes.length; i++) { %><option value=<%= availableAttributes[i]%>><%= availableAttributes[i]%></option><% } %></select></div><div class = "col-xs-3"><select name="op-1" class = "form-control"><% for(var i=0; i<conditions.length; i++) { %><option value=<%= conditions[i]%>><%= conditions[i]%></option><% } %></select></div><div class = "col-xs-3"><input type="text" class="form-control" name="condValue-1" data-bind="value: toTime"></div><div class = "col-xs-3"><select name="combiner-1" class = "form-control"><option value="AND">AND</option><option value=“OR">OR</option></select></div></div>';
+
+
+
+    jQuery('#formContainer').append(row2);
     //frm.add_qty.value = '';
     //frm.add_name.value = '';
 }
