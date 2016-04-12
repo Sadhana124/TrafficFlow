fddc6f2165c02da3d7f5d5b9658d3abdabc9959a
Sadhana124 <sadhana.412@gmail.com>
2016-04-11 20:18:15 -0700

    added admin to routes
    also included admin ejs

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,12 @@
 /**
  * Created by SadhanaRamachandran on 4/11/16.
  */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('admin', { title: 'Express' });
+});
+
+module.exports = router;
