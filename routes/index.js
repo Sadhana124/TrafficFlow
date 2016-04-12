8028a633818f524f0721b738922b69cadf167c74
Sadhana124 <sadhana.412@gmail.com>
2016-04-11 22:06:01 -0700

    adding checkboxes for select attributes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+data = {
+    title: 'Network FLow Query',
+    availableAttributes : ['Destination ip', 'Destination vn', 'Direction ingress',
+    'Destination port', 'Protocol', 'Source ip', 'Source vn', 'Source port', 'Sum of bytes', 'Sum of packets'],
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Network FLow Query' });
+  res.render('index', data);
 });
 
 module.exports = router;
