e1d2a7c4b3392aff7f88ac95c5e49ade0cb0f4f4
Sadhana124 <sadhana.412@gmail.com>
2016-04-11 21:08:01 -0700

    including the required js libs and bootstrap

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/admin', admin);
+app.use('/scripts', express.static(__dirname+ '/public/javascripts/'))
+app.use('/bootstrap', express.static(__dirname+ '/public/stylesheets/bootstrap/'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
